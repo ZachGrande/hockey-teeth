9d05297c7519f9ffefe5252d2e8c94ab62c946cc
Zach Grande <zach.grande23@gmail.com>
2023-01-29 14:40:43 -0800

    Added Spotify, Apple Music, Bandcamp links

--- ui/hockey-teeth/src/App.tsx
+++ ui/hockey-teeth/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './assets/logo.png';
-import instagram from './assets/instagram.svg'
+import instagram from './assets/instagram.svg';
+import spotify from './assets/spotify.svg';
+import apple from './assets/apple-music.svg';
+import bandcamp from './assets/bandcamp.svg';
 import './App.css';
 import {Grid} from "@mui/material";
 
@@ -18,9 +21,31 @@ function App() {
         }}
       >
         <Grid item xs={8}>
-          <a href={"https://www.instagram.com/hockeyteethband/"}>
+          <a href={"https://www.instagram.com/hockeyteethband/"}
+             target="_blank"
+             rel="noreferrer">
             <img src={instagram} alt="instagram" />
-            <p>Visit our Instagram</p>
+          </a>
+        </Grid>
+        <Grid item xs={8}>
+          <a href={"https://open.spotify.com/artist/2Flj4MtGAxNIW1Ahutwa37?si=Rzc-IdzeQvGLkFKLytPU_Q/"}
+             target="_blank"
+             rel="noreferrer">
+            <img src={spotify} alt="spotify" />
+          </a>
+        </Grid>
+        <Grid item xs={8}>
+          <a href={"https://music.apple.com/us/artist/hockey-teeth/1529979055"}
+             target="_blank"
+             rel="noreferrer">
+            <img src={apple} alt="apple-music" />
+          </a>
+        </Grid>
+        <Grid item xs={8}>
+          <a href={"https://hockeyteeth1.bandcamp.com/"}
+             target="_blank"
+             rel="noreferrer">
+            <img src={bandcamp} alt="band-camp" />
           </a>
         </Grid>
       </Grid>
