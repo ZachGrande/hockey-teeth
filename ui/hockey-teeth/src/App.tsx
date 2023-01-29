bb5ab47c4e07e2abfa7d26dcea61d101279c7c9f
Zach Grande <zach.grande23@gmail.com>
2023-01-29 13:02:50 -0800

    Styled Instagram link

--- ui/hockey-teeth/src/App.tsx
+++ ui/hockey-teeth/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './assets/logo.png';
 import instagram from './assets/instagram.svg'
 import './App.css';
+import {Grid} from "@mui/material";
 
 function App() {
   return (
@@ -9,12 +10,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <div className="social">
-        <a href={"https://www.instagram.com/hockeyteethband/"}>
-          <img src={instagram} alt="instagram" />
-          Visit our Instagram
-        </a>
-      </div>
+      <Grid container className="social" spacing={2}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Grid item xs={8}>
+          <a href={"https://www.instagram.com/hockeyteethband/"}>
+            <img src={instagram} alt="instagram" />
+            <p>Visit our Instagram</p>
+          </a>
+        </Grid>
+      </Grid>
     </div>
   );
 }
