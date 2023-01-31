0f182b1505990653be87f428f40b98c75dc1bdf7
Zach Grande <zach.grande23@gmail.com>
2023-01-30 20:38:10 -0800

    Extracted socials into component

--- ui/hockey-teeth/src/App.tsx
+++ ui/hockey-teeth/src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import logo from './assets/logo.png';
-import instagram from './assets/instagram.svg';
-import spotify from './assets/spotify.svg';
-import apple from './assets/apple-music.svg';
-import bandcamp from './assets/bandcamp.svg';
 import './App.css';
-import {Stack} from "@mui/material";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
@@ -13,34 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <Stack className="social" direction="row" spacing={2}
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <a href={"https://www.instagram.com/hockeyteethband/"}
-           target="_blank"
-           rel="noreferrer">
-          <img src={instagram} alt="instagram" />
-        </a>
-        <a href={"https://open.spotify.com/artist/2Flj4MtGAxNIW1Ahutwa37?si=Rzc-IdzeQvGLkFKLytPU_Q/"}
-           target="_blank"
-           rel="noreferrer">
-          <img src={spotify} alt="spotify" />
-        </a>
-        <a href={"https://music.apple.com/us/artist/hockey-teeth/1529979055"}
-           target="_blank"
-           rel="noreferrer">
-          <img src={apple} alt="apple-music" />
-        </a>
-        <a href={"https://hockeyteeth1.bandcamp.com/"}
-           target="_blank"
-           rel="noreferrer">
-          <img src={bandcamp} alt="band-camp" />
-        </a>
-      </Stack>
+      <SocialLinks />
     </div>
   );
 }
