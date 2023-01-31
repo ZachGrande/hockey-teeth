3afd98d7667763b7c59d9cdd048531a124ed4ab9
Zach Grande <zach.grande23@gmail.com>
2023-01-30 20:26:34 -0800

    Adjusted social icons to horizontal alignment

--- ui/hockey-teeth/src/App.tsx
+++ ui/hockey-teeth/src/App.tsx
@@ -5,7 +5,7 @@ import spotify from './assets/spotify.svg';
 import apple from './assets/apple-music.svg';
 import bandcamp from './assets/bandcamp.svg';
 import './App.css';
-import {Grid} from "@mui/material";
+import {Stack} from "@mui/material";
 
 function App() {
   return (
@@ -13,42 +13,34 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <Grid container className="social" spacing={2}
+      <Stack className="social" direction="row" spacing={2}
         sx={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center"
         }}
       >
-        <Grid item xs={8}>
-          <a href={"https://www.instagram.com/hockeyteethband/"}
-             target="_blank"
-             rel="noreferrer">
-            <img src={instagram} alt="instagram" />
-          </a>
-        </Grid>
-        <Grid item xs={8}>
-          <a href={"https://open.spotify.com/artist/2Flj4MtGAxNIW1Ahutwa37?si=Rzc-IdzeQvGLkFKLytPU_Q/"}
-             target="_blank"
-             rel="noreferrer">
-            <img src={spotify} alt="spotify" />
-          </a>
-        </Grid>
-        <Grid item xs={8}>
-          <a href={"https://music.apple.com/us/artist/hockey-teeth/1529979055"}
-             target="_blank"
-             rel="noreferrer">
-            <img src={apple} alt="apple-music" />
-          </a>
-        </Grid>
-        <Grid item xs={8}>
-          <a href={"https://hockeyteeth1.bandcamp.com/"}
-             target="_blank"
-             rel="noreferrer">
-            <img src={bandcamp} alt="band-camp" />
-          </a>
-        </Grid>
-      </Grid>
+        <a href={"https://www.instagram.com/hockeyteethband/"}
+           target="_blank"
+           rel="noreferrer">
+          <img src={instagram} alt="instagram" />
+        </a>
+        <a href={"https://open.spotify.com/artist/2Flj4MtGAxNIW1Ahutwa37?si=Rzc-IdzeQvGLkFKLytPU_Q/"}
+           target="_blank"
+           rel="noreferrer">
+          <img src={spotify} alt="spotify" />
+        </a>
+        <a href={"https://music.apple.com/us/artist/hockey-teeth/1529979055"}
+           target="_blank"
+           rel="noreferrer">
+          <img src={apple} alt="apple-music" />
+        </a>
+        <a href={"https://hockeyteeth1.bandcamp.com/"}
+           target="_blank"
+           rel="noreferrer">
+          <img src={bandcamp} alt="band-camp" />
+        </a>
+      </Stack>
     </div>
   );
 }
