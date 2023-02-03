a397d03dba32c8fd1268c2d72d7e13c80606fb09
Zach Grande <zach.grande23@gmail.com>
2023-02-02 20:32:25 -0800

    Addded music video page

---
+++ ui/hockey-teeth/src/pages/MusicVideos.tsx
@@ -0,0 +1,26 @@
+function MusicVideos() {
+  return (
+    <div
+      style={{
+        position: 'relative',
+        display: 'flex',
+        paddingBottom: '56.25%',
+        height: 0,
+        overflow: 'hidden',
+        justifyContent: 'center'
+      }}
+    >
+      <iframe
+        width="90%" height="90%"
+        title="Isolation"
+        src="https://www.youtube.com/embed/8kFYLvi7u0o"
+        style={{
+          position: 'absolute'
+        }}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen />
+    </div>
+  );
+}
+
+export default MusicVideos;
--- ui/hockey-teeth/src/routers/SiteRouter.tsx
+++ ui/hockey-teeth/src/routers/SiteRouter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Navigate, Route, Routes} from "react-router-dom";
 import HomeLayout from "../layouts/HomeLayout";
 import Shows from "../pages/Shows";
+import MusicVideos from "../pages/MusicVideos";
 
 function SiteRouter() {
   return (
@@ -9,6 +10,7 @@ function SiteRouter() {
       <Route element={<HomeLayout />}>
         <Route index element={<Navigate replace to="/home" />}/>
         <Route path="home" element={<Shows />}/>
+        <Route path="videos" element={<MusicVideos />}/>
       </Route>
       <Route path="*" element={<Navigate replace to="/home" />}/>
     </Routes>
