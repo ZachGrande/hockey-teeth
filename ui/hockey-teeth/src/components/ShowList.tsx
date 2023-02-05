6d16bb19112500b35d111fafab3e144716e57dbb
Zach Grande <zach.grande23@gmail.com>
2023-02-04 17:46:45 -0800

    Extracted shows list into separate file

--- ui/hockey-teeth/src/components/ShowList.tsx
+++ ui/hockey-teeth/src/components/ShowList.tsx
@@ -1,25 +1,8 @@
 import React from "react";
 import {Link, Typography} from "@mui/material";
+import {shows} from '../data/Shows';
 
 function ShowList() {
-  const shows = [
-    {
-      date: "Feb 5, 2023",
-      venue: "Factory Luxe",
-      location: "Seattle, WA",
-      link: "https://www.thefactoryluxe.com/events/hockey-teeth-i90-fiasco-choly?fbclid=PAAaZVlXVZ-pjc-N1vANQXYW7J" +
-            "2uZ5_kHd-ez2jwshJBpT_NvCLNsSnaKyIyE",
-      linkIncluded: true
-    },
-    {
-      date: "Feb 24, 2023",
-      venue: "The Blue Room",
-      location: "Bellingham, WA",
-      link: "",
-      linkIncluded: false
-    }
-  ];
-
   return (
     <div className="show-list">
       {shows.map((data, key) => {
---
+++ ui/hockey-teeth/src/data/Shows.js
@@ -0,0 +1,17 @@
+export const shows = [
+  {
+    date: "Feb 5, 2023",
+    venue: "Factory Luxe",
+    location: "Seattle, WA",
+    link: "https://www.thefactoryluxe.com/events/hockey-teeth-i90-fiasco-choly?fbclid=PAAaZVlXVZ-pjc-N1vANQXYW7J" +
+    "2uZ5_kHd-ez2jwshJBpT_NvCLNsSnaKyIyE",
+    linkIncluded: true
+  },
+  {
+    date: "Feb 24, 2023",
+    venue: "The Blue Room",
+    location: "Bellingham, WA",
+    link: "",
+    linkIncluded: false
+  }
+];
