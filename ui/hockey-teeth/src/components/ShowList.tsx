5ac3202a3e0022d480e268d0043d1efd7097cbc8
Zach Grande <zach.grande23@gmail.com>
2023-02-04 17:42:52 -0800

    Extracted shows list into JSON

--- ui/hockey-teeth/src/components/ShowList.tsx
+++ ui/hockey-teeth/src/components/ShowList.tsx
@@ -2,20 +2,43 @@ import React from "react";
 import {Link, Typography} from "@mui/material";
 
 function ShowList() {
+  const shows = [
+    {
+      date: "Feb 5, 2023",
+      venue: "Factory Luxe",
+      location: "Seattle, WA",
+      link: "https://www.thefactoryluxe.com/events/hockey-teeth-i90-fiasco-choly?fbclid=PAAaZVlXVZ-pjc-N1vANQXYW7J" +
+            "2uZ5_kHd-ez2jwshJBpT_NvCLNsSnaKyIyE",
+      linkIncluded: true
+    },
+    {
+      date: "Feb 24, 2023",
+      venue: "The Blue Room",
+      location: "Bellingham, WA",
+      link: "",
+      linkIncluded: false
+    }
+  ];
+
   return (
     <div className="show-list">
-      <div className="show-item">
-        <Typography variant="h4">Feb 5, 2023 | Factory Luxe</Typography>
-        <Typography variant="body1">{"Seattle, WA | "}
-          <Link variant="body1" color="inherit" href="https://www.thefactoryluxe.com/events/hockey-teeth-i90-fiasco-choly?fbclid=PAAaZVlXVZ-pjc-N1vANQXYW
-                   7J2uZ5_kHd-ez2jwshJBpT_NvCLNsSnaKyIyE" target="_blank" rel="noreferrer">Tickets</Link>
-        </Typography>
-      </div>
-      <br />
-      <div className="show-item">
-        <Typography variant="h4">Feb 24, 2023 | The Blue Room</Typography>
-        <Typography variant="body1">Bellingham, WA</Typography>
-      </div>
+      {shows.map((data, key) => {
+        const subHeader = data.linkIncluded ?
+          <Typography variant="body1">{data.location + " | "}
+            <Link variant="body1" color="inherit" href={data.link} target="_blank" rel="noreferrer">Tickets</Link>
+          </Typography>
+          :
+          <Typography variant="body1">{data.location}</Typography>
+        return (
+          <div key={key}>
+            <div className="show-item">
+              <Typography variant="h4">{data.date + " | " + data.venue}</Typography>
+              {subHeader}
+            </div>
+            <br />
+          </div>
+        );
+      })}
     </div>
   );
 }
