22e0b9ebefb56c4db96a5a40a39770f5d976defc
Zach Grande <zach.grande23@gmail.com>
2023-02-07 17:24:51 -0800

    Added Blue Room to upcoming shows

--- ui/hockey-teeth/src/data/Shows.js
+++ ui/hockey-teeth/src/data/Shows.js
@@ -1,9 +1,16 @@
 export const shows = [
   {
     date: "Feb 24, 2023",
-    venue: "The Blue Room",
+    venue: "The Blue Room w/ Kazmyn",
     location: "Bellingham, WA",
     link: "",
     linkIncluded: false
+  },
+  {
+    date: "Mar 25, 2023",
+    venue: "The Blue Room",
+    location: "Bellingham, WA",
+    link: "https://ticketbud.com/events/02db75a6-92bb-11ed-a500-42010a71701e",
+    linkIncluded: true
   }
 ];
