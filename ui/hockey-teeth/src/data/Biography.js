2c2eec8b43847ffe219b4291ddb8349a358e155a
Zach Grande <zach.grande23@gmail.com>
2023-02-07 18:08:58 -0800

    Added MUI masonry image list to bio page, added margins to bio paragraph

--- ui/hockey-teeth/src/data/Biography.js
+++ ui/hockey-teeth/src/data/Biography.js
@@ -1,5 +1,7 @@
 export const biography = {
-  bio: "Blending together the high energy of modern pop with the catchy melodies of classic rock, Hockey Teeth aims to redefine Alternative Pop in the Pacific Northwest. After winning WWU’s 2020 Battle of the Bands in Bellingham and gaining a steady local following, they are breaking into the Seattle scene with their newest single \"Isolation.\" The track showcases a more reserved sound for the band, with a thoughtful instrumental carried by acoustic guitar and keyboards.",
-  author: "The Western Front",
-  link: "https://www.westernfrontonline.com/article/2022/06/long-awaited-lawnstock-makes-first-in-person-return-since-2019"
+  bio: "Blending together the high energy of modern pop with the catchy melodies of classic rock, Hockey Teeth aims " +
+    "to redefine Alternative Pop in the Pacific Northwest. After winning WWU’s 2020 Battle of the Bands in " +
+    "Bellingham and gaining a steady local following, they are breaking into the Seattle scene with their newest " +
+    "single \"Isolation.\" The track showcases a more reserved sound for the band, with a thoughtful instrumental " +
+    "carried by acoustic guitar and keyboards.",
 }
--- ui/hockey-teeth/src/pages/Biography.tsx
+++ ui/hockey-teeth/src/pages/Biography.tsx
@@ -1,9 +1,60 @@
 import React from "react";
-import {Link, Typography} from "@mui/material";
+import {ImageList, ImageListItem, Link, Typography} from "@mui/material";
 import {biography} from "../data/Biography";
-import bamf from "../assets/bamf.png";
+import bamf from "../assets/band-photos/bamf.png";
+import photo1 from "../assets/band-photos/band-01.png";
+import photo2 from "../assets/band-photos/band-02.png";
+import photo3 from "../assets/band-photos/band-03.png";
+import photo4 from "../assets/band-photos/band-04.png";
+import photo5 from "../assets/band-photos/band-05.png";
+import photo6 from "../assets/band-photos/band-06.png";
 
 function Biography() {
+  const images = [
+    {
+      title: "Photo 1",
+      image: photo1,
+      cols: 2,
+      rows: 1,
+      width: "10px"
+    },
+    {
+      title: "Photo 2",
+      image: photo2,
+      cols: 2,
+      rows: 2,
+      width: "64px"
+    },
+    {
+      title: "Photo 3",
+      image: photo3,
+      cols: 2,
+      rows: 2,
+      width: "64px"
+    },
+    {
+      title: "Photo 4",
+      image: photo4,
+      cols: 1,
+      rows: 2,
+      width: "64px"
+    },
+    {
+      title: "Photo 5",
+      image: photo5,
+      cols: 1,
+      rows: 2,
+      width: "64px"
+    },
+    {
+      title: "Photo 6",
+      image: photo6,
+      cols: 2,
+      rows: 1,
+      width: "64px"
+    },
+  ]
+
   return (
     <>
       <div
@@ -32,12 +83,34 @@ function Biography() {
         Photo courtesy of @scottstarkphoto.
       </Link>
       <br />
+      <br />
       <Typography variant="h3">
         About Hockey Teeth
       </Typography>
-      <Typography variant="body1">
+      <Typography variant="body1" sx={{ margin: 5 }}>
         {biography.bio}
       </Typography>
+      <ImageList
+        sx={{
+          margin: 5,
+        }}
+        variant="masonry"
+        cols={3}
+        gap={8}
+      >
+        {images.map((item) => (
+          <ImageListItem
+            key={item.image}
+            cols={item.cols || 1}
+            rows={item.rows || 1}>
+            <img
+              src={item.image}
+              alt={item.title}
+              loading="lazy"
+          />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </>
   );
 }
