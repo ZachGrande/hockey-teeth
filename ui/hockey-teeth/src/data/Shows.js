d0b18e38116813b92debe737ddf9ee58c0a5eeca
Zach Grande <zach.grande23@gmail.com>
2023-03-02 17:45:20 -0800

    Added upcoming shows

--- ui/hockey-teeth/src/data/Shows.js
+++ ui/hockey-teeth/src/data/Shows.js
@@ -5,5 +5,26 @@ export const shows = [
     location: "Bellingham, WA",
     link: "https://ticketbud.com/events/02db75a6-92bb-11ed-a500-42010a71701e",
     linkIncluded: true
+  },
+  {
+    date: "Apr 23, 2023",
+    venue: "Skagit Valley Tulip Festival",
+    location: "Mount Vernon, WA",
+    link: "https://tuliptown.com/pages/tickets",
+    linkIncluded: true
+  },
+  {
+    date: "May 3, 2023",
+    venue: "Sunset Tavern",
+    location: "Seattle, WA",
+    link: "",
+    linkIncluded: false
+  },
+  {
+    date: "May 4, 2023",
+    venue: "The Shakedown",
+    location: "Bellingham, WA",
+    link: "",
+    linkIncluded: false
   }
 ];
