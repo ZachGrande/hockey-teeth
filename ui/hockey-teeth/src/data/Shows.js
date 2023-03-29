1495fb41f96a8df7a3f4910b0cf10ab22e6e424a
Zach Grande <zach.grande23@gmail.com>
2023-03-29 10:35:49 -0700

    Moved Blue Room to past shows, added new show

--- ui/hockey-teeth/src/data/Shows.js
+++ ui/hockey-teeth/src/data/Shows.js
@@ -1,12 +1,5 @@
 export const shows = [
   {
-    date: "Mar 25, 2023",
-    venue: "The Blue Room",
-    location: "Bellingham, WA",
-    link: "https://ticketbud.com/events/02db75a6-92bb-11ed-a500-42010a71701e",
-    linkIncluded: true
-  },
-  {
     date: "Apr 23, 2023",
     venue: "Skagit Valley Tulip Festival",
     location: "Mount Vernon, WA",
@@ -23,6 +16,13 @@ export const shows = [
   {
     date: "May 4, 2023",
     venue: "The Shakedown",
+    location: "Bellingham, WA",
+    link: "",
+    linkIncluded: false
+  },
+  {
+    date: "May 26, 2023",
+    venue: "The Blue Room",
     location: "Bellingham, WA",
     link: "",
     linkIncluded: false
--- ui/hockey-teeth/src/data/ShowsPast.js
+++ ui/hockey-teeth/src/data/ShowsPast.js
@@ -1,5 +1,12 @@
 export const shows = [
   {
+    date: "Mar 25, 2023",
+    venue: "The Blue Room",
+    location: "Bellingham, WA",
+    link: "",
+    linkIncluded: false
+  },
+  {
     date: "Feb 24, 2023",
     venue: "The Blue Room w/ Kazmyn",
     location: "Bellingham, WA",
