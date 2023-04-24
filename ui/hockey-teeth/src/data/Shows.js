b19d0585b85438e6b0acc1cc7048ee861d4646c4
Zach Grande <zach.grande23@gmail.com>
2023-04-24 10:05:49 -0700

    Added tickets to some upcoming shows

--- ui/hockey-teeth/src/data/Shows.js
+++ ui/hockey-teeth/src/data/Shows.js
@@ -3,15 +3,15 @@ export const shows = [
     date: "May 3, 2023",
     venue: "Sunset Tavern",
     location: "Seattle, WA",
-    link: "",
-    linkIncluded: false
+    link: "https://www.ticketweb.com/event/american-flats-model-shop-hockey-sunset-tavern-tickets/13119055?pl=sunset",
+    linkIncluded: true
   },
   {
     date: "May 4, 2023",
     venue: "The Shakedown",
     location: "Bellingham, WA",
-    link: "",
-    linkIncluded: false
+    link: "https://shakedownbellingham.com/#/events?event_id=73757",
+    linkIncluded: true
   },
   {
     date: "May 26, 2023",
