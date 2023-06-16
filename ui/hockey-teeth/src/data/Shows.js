3b0c60ef9dc61131313a51984c169df854006438
Zach Grande <zach.grande23@gmail.com>
2023-06-16 13:35:38 -0700

    Edited shows

--- ui/hockey-teeth/src/data/Shows.js
+++ ui/hockey-teeth/src/data/Shows.js
@@ -3,13 +3,20 @@ export const shows = [
     date: "July 6, 2023",
     venue: "The Wild Buffalo",
     location: "Bellingham, WA",
-    link: "",
-    linkIncluded: false
+    link: "https://wildbuffalo.net/#/events?event_id=75586",
+    linkIncluded: true
   },
   {
     date: "July 21, 2023",
     venue: "The Shakedown",
     location: "Bellingham, WA",
+    link: "https://shakedownbellingham.com/#/events?event_id=77716",
+    linkIncluded: true
+  },
+  {
+    date: "Aug 25, 2023",
+    venue: "Lucky Dime",
+    location: "Everett, WA",
     link: "",
     linkIncluded: false
   }
