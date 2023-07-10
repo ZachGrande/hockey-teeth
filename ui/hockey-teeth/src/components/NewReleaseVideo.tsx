e6b0a27c32102af6bb804811b7434a2c35737864
Zach Grande <zach.grande23@gmail.com>
2023-07-09 17:24:04 -0700

    Removed unused variable from NewReleaseVideo

--- ui/hockey-teeth/src/components/NewReleaseVideo.tsx
+++ ui/hockey-teeth/src/components/NewReleaseVideo.tsx
@@ -2,33 +2,6 @@ import {Typography} from "@mui/material";
 import React from "react";
 
 function Videos() {
-    const videos = [
-        {
-            title: "KSPU Session",
-            link: "https://www.youtube.com/embed/tUz4Hmn6lAI"
-        },
-        {
-            title: "Freshwater Session",
-            link: "https://www.youtube.com/embed/FVeE-bCO3XQ"
-        },
-        {
-            title: "Isolation",
-            link: "https://www.youtube.com/embed/8kFYLvi7u0o"
-        },
-        {
-            title: "Go There",
-            link: "https://www.youtube.com/embed/BlcXgZdIpms"
-        },
-        {
-            title: "Behind The Scenes",
-            link: "https://www.youtube.com/embed/VxBGSGYzP8Y"
-        },
-        {
-            title: "Pocket Concerts",
-            link: "https://www.youtube.com/embed/i3d9LylY_cc"
-        }
-    ]
-
     return (
         <>
             <Typography variant="h1">Sick Of Me Music Video Out Now!</Typography>
