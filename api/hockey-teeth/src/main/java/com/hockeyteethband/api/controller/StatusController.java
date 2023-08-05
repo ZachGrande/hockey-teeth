6ce1843e8f2da3d78f226990e0ba3fa0d4a74fb9
Zach Grande <zach.grande23@gmail.com>
2023-08-05 12:03:08 -0700

    Added logging to API status endpoint

--- api/hockey-teeth/src/main/java/com/hockeyteethband/api/controller/StatusController.java
+++ api/hockey-teeth/src/main/java/com/hockeyteethband/api/controller/StatusController.java
@@ -2,12 +2,16 @@ package com.hockeyteethband.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class StatusController {
+    private static final Logger logger = LoggerFactory.getLogger(StatusController.class);
 
     @GetMapping("/status")
     public String getStatus() {
+        logger.info("Returning status");
         return "{\"status\": \"up\"}";
     }
 
