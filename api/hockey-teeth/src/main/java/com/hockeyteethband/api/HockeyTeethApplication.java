dea5533212ee9356f92c8cb23c6db4df5046637d
Zach Grande <zach.grande23@gmail.com>
2023-08-04 17:08:31 -0700

    Initialized Spring Boot API project

---
+++ api/hockey-teeth/src/main/java/com/hockeyteethband/api/HockeyTeethApplication.java
@@ -0,0 +1,13 @@
+package com.hockeyteethband.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeyTeethApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HockeyTeethApplication.class, args);
+  }
+
+}
---
+++ api/hockey-teeth/src/test/java/com/hockeyteethband/api/HockeyTeethApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hockeyteethband.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeyTeethApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
