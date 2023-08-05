6129a1dca56a89dfc7d929f40f7f6ef8f8be23cc
Zach Grande <zach.grande23@gmail.com>
2023-08-05 11:54:03 -0700

    Return API health for local development

--- ui/hockey-teeth/src/components/HealthStatus.tsx
+++ ui/hockey-teeth/src/components/HealthStatus.tsx
@@ -1,6 +1,54 @@
+import {useEffect, useState} from "react";
+import config from "../../src/config";
+import {Box, CircularProgress, Typography} from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ErrorIcon from "@mui/icons-material/Error";
+
 const HealthStatus = () => {
+    const [status, setStatus] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        fetch(`${config.api.apiUrl}/status`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Error fetching status");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setStatus(data.status);
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(true);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <CircularProgress />;
+    }
+
+    if (error || status !== "up") {
+        return (
+            <Box>
+                <ErrorIcon color="error" />
+                <Typography variant="body1" color="error">
+                    API is not available
+                </Typography>
+            </Box>
+        )
+    }
+
     return (
-        <p>Hello!</p>
+        <Box>
+            <CheckCircleIcon color="success" />
+            <Typography variant="body1">
+                API is available
+            </Typography>
+        </Box>
     )
 }
 
--- ui/hockey-teeth/src/config.js
+++ ui/hockey-teeth/src/config.js
@@ -2,6 +2,9 @@ const config = {
     auth: {
         domain: "hockeyteeth.us.auth0.com",
         clientId: "1mwH4FXs3RwDQFBv0dR6cTnKj4UWWukd"
+    },
+    api: {
+        apiUrl: "http://localhost:8080"
     }
 };
 
