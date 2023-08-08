42701d99d435cdfcfe6fbdc386f5c0f80efcee1d
Zach Grande <zach.grande23@gmail.com>
2023-08-08 15:21:02 -0700

    Added AirBnB eslint preferences

--- ui/hockey-teeth/.eslintrc.js
+++ ui/hockey-teeth/.eslintrc.js
@@ -3,11 +3,16 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript'
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json"
   },
   rules: {
     'no-console': ['error', { allow: ['warn', 'error'] }],
