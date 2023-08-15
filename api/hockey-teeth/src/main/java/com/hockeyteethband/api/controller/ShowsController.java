d59cf84aaf04ff0427cc78f09c1565391b427f2e
Zach Grande <zach.grande23@gmail.com>
2023-08-14 19:59:37 -0700

    Add endpoints to filter past and upcoming shows

--- api/hockey-teeth/src/main/java/com/hockeyteethband/api/controller/ShowsController.java
+++ api/hockey-teeth/src/main/java/com/hockeyteethband/api/controller/ShowsController.java
@@ -1,7 +1,7 @@
 package com.hockeyteethband.api.controller;
 
 import com.hockeyteethband.api.model.Show;
-import com.hockeyteethband.api.repository.ShowsRepository;
+import com.hockeyteethband.api.service.ShowService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +14,21 @@ import java.util.List;
 @RequestMapping("/shows")
 public class ShowsController {
 
-    private final ShowsRepository showsRepository;
+    private final ShowService showService;
 
-    public ShowsController(ShowsRepository showsRepository) {
-        this.showsRepository = showsRepository;
+    public ShowsController(ShowService showService) {
+        this.showService = showService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<Show>> getAllShows() {
-        List<Show> shows = showsRepository.getAllShows();
+    @GetMapping("/past")
+    public ResponseEntity<List<Show>> getAllPastShows() {
+        List<Show> shows = showService.getAllPastShows();
+        return new ResponseEntity<>(shows, HttpStatus.OK);
+    }
+
+    @GetMapping("/upcoming")
+    public ResponseEntity<List<Show>> getAllUpcomingShows() {
+        List<Show> shows = showService.getAllUpcomingShows();
         return new ResponseEntity<>(shows, HttpStatus.OK);
     }
 }
---
+++ api/hockey-teeth/src/main/java/com/hockeyteethband/api/service/ShowService.java
@@ -0,0 +1,31 @@
+package com.hockeyteethband.api.service;
+
+import com.hockeyteethband.api.model.Show;
+import com.hockeyteethband.api.repository.ShowsRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ShowService {
+
+    private final ShowsRepository showsRepository;
+
+    public ShowService(ShowsRepository showsRepository) {
+        this.showsRepository = showsRepository;
+    }
+
+    public List<Show> getAllPastShows() {
+        return showsRepository.getAllShows().stream()
+            .filter(show -> LocalDate.parse(show.getDate()).isBefore(LocalDate.now()))
+            .collect(Collectors.toList());
+    }
+
+    public List<Show> getAllUpcomingShows() {
+        return showsRepository.getAllShows().stream()
+            .filter(show -> LocalDate.parse(show.getDate()).isAfter(LocalDate.now()))
+            .collect(Collectors.toList());
+    }
+}
