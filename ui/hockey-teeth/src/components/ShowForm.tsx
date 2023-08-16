bcc068dafceffffa173d446878d8db372f0f4951
Zach Grande <zach.grande23@gmail.com>
2023-08-15 18:37:14 -0700

    Implemented form validation with Yup and Formik

--- ui/hockey-teeth/src/components/ShowForm.tsx
+++ ui/hockey-teeth/src/components/ShowForm.tsx
@@ -3,78 +3,108 @@ import axios from 'axios';
 import {
   Alert, Button, Container, Grid, Stack, TextField, Typography,
 } from '@mui/material';
+import { useFormik, FormikErrors } from 'formik';
+import * as yup from 'yup';
 import config from '../config';
 
+type FormValues = {
+  date: string;
+  venue: string;
+  location: string;
+  link: string;
+};
+
+type ExtendedErrors = FormikErrors<FormValues> & { submit?: string };
+
+const validationSchema = yup.object({
+  date: yup.string().required('Date is required').matches(/^\d{4}-\d{2}-\d{2}$/, 'Date must be in YYYY-MM-DD format'),
+  venue: yup.string().required('Venue is required'),
+  location: yup.string().required('Location is required'),
+  link: yup.string().url('Enter a valid URL'),
+});
+
 function ShowForm() {
-  const [date, setDate] = useState('');
-  const [venue, setVenue] = useState('');
-  const [location, setLocation] = useState('');
-  const [link, setLink] = useState('');
-  const [success, setSuccess] = useState('');
-  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      date: '',
+      venue: '',
+      location: '',
+      link: '',
+    },
+    validationSchema,
+    onSubmit: async (values, { setSubmitting, setErrors }) => {
+      try {
+        const response = await axios.post(`${config.api.apiUrl}/shows/add`, values, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
 
-  const handleSubmit = async (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-    setSuccess('');
-    setError('');
-
-    const data = {
-      date, venue, location, link,
-    };
-
-    try {
-      const response = await axios.post(`${config.api.apiUrl}/shows/add`, data, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (response.status === 201) {
-        setSuccess('Show added successfully!');
-      } else {
-        setError('An error occurred while adding the show.');
+        if (response.status !== 201) {
+          setErrors({ submit: 'An error occurred while adding the show.' } as ExtendedErrors);
+        } else {
+          setSuccess(true);
+          formik.resetForm();
+        }
+      } catch (e) {
+        setErrors({ submit: 'An error occurred while adding the show.' } as ExtendedErrors);
       }
-    } catch (e) {
-      setError('An error occurred while adding the show.');
-    }
-  };
+
+      setSubmitting(false);
+    },
+  });
+
+  const formErrors = formik.errors as ExtendedErrors;
 
   return (
     <Container>
-      <Typography variant="h3" sx={{ mb: 2 }}>Add Show</Typography>
-      <form onSubmit={handleSubmit}>
+      <Typography variant="h3" gutterBottom>Add Show</Typography>
+      <form onSubmit={formik.handleSubmit}>
         <Stack spacing={3}>
           <Grid container spacing={3} justifyContent="center">
             <Grid item xs={12} sm={8} md={6}>
               <TextField
-                required
-                label="Date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
+                label="Date (e.g. 2023-08-14)"
+                name="date"
+                value={formik.values.date}
+                onChange={formik.handleChange}
+                error={formik.touched.date && Boolean(formik.errors.date)}
+                helperText={formik.touched.date && formik.errors.date}
+                fullWidth
               />
             </Grid>
             <Grid item xs={12} sm={8} md={6}>
               <TextField
-                required
                 label="Venue"
-                value={venue}
-                onChange={(e) => setVenue(e.target.value)}
+                name="venue"
+                value={formik.values.venue}
+                onChange={formik.handleChange}
+                error={formik.touched.venue && Boolean(formik.errors.venue)}
+                helperText={formik.touched.venue && formik.errors.venue}
+                fullWidth
               />
             </Grid>
             <Grid item xs={12} sm={8} md={6}>
               <TextField
-                required
-                label="Link"
-                value={link}
-                onChange={(e) => setLink(e.target.value)}
+                label="Link (Optional)"
+                name="link"
+                value={formik.values.link}
+                onChange={formik.handleChange}
+                error={formik.touched.link && Boolean(formik.errors.link)}
+                helperText={formik.touched.link && formik.errors.link}
+                fullWidth
               />
             </Grid>
             <Grid item xs={12} sm={8} md={6}>
               <TextField
-                required
                 label="Location"
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
+                name="location"
+                value={formik.values.location}
+                onChange={formik.handleChange}
+                error={formik.touched.location && Boolean(formik.errors.location)}
+                helperText={formik.touched.location && formik.errors.location}
+                fullWidth
               />
             </Grid>
             <Grid item xs={12} sm={8} md={6}>
@@ -82,20 +112,18 @@ function ShowForm() {
                 Submit
               </Button>
             </Grid>
-            {error && (
-              <Grid item xs={12} sm={8} md={6}>
-                <Alert severity="error">
-                  {error}
-                </Alert>
-              </Grid>
-            )}
-            {success && (
-              <Grid item xs={12} sm={8} md={6}>
+            <Grid item xs={12} sm={8} md={6}>
+              {formErrors.submit && (
+              <Alert severity="error">
+                {formErrors.submit}
+              </Alert>
+              )}
+              {success && (
                 <Alert severity="success">
-                  {success}
+                  Show added successfully!
                 </Alert>
-              </Grid>
-            )}
+              )}
+            </Grid>
           </Grid>
         </Stack>
       </form>
