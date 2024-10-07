7671f78dd0bb704c2b1814ace9f5fb920cc10ebf
Zach Grande <zach.grande23@gmail.com>
2024-10-06 22:04:10 -0700

    feat(infra): build cloudformationstack

--- infrastructure/src/main/java/com/myorg/InfrastructureStack.java
+++ infrastructure/src/main/java/com/myorg/InfrastructureStack.java
@@ -1,10 +1,17 @@
 package com.myorg;
 
+import java.util.List;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.ecs.Protocol;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.FargateTaskDefinition;
+import software.amazon.awscdk.services.ecs.FargateTaskDefinitionProps;
+import software.amazon.awscdk.services.ecs.PortMapping;
+import software.amazon.awscdk.services.ecs.ContainerDefinitionOptions;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.FargateService;
 
 public class InfrastructureStack extends Stack {
     public InfrastructureStack(final Construct scope, final String id) {
@@ -14,11 +21,36 @@ public class InfrastructureStack extends Stack {
     public InfrastructureStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        Cluster cluster = new Cluster(this, "HockeyTeethAPICluster");
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "InfrastructureQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        FargateTaskDefinitionProps taskDefinitionProps =
+            FargateTaskDefinitionProps.builder()
+                .memoryLimitMiB(512)
+                .cpu(256)
+                .build();
+
+        FargateTaskDefinition taskDefinition = 
+            new FargateTaskDefinition(this,
+                                      "HockeyTeethAPITaskDefinition",
+                                      taskDefinitionProps);
+
+        ContainerDefinitionOptions containerDefinitionOptions = ContainerDefinitionOptions.builder()
+            .image(ContainerImage.fromRegistry("hockeyteeth-api"))
+            .portMappings(List.of(
+                PortMapping.builder()
+                    .containerPort(8080)
+                    .protocol(Protocol.TCP)
+                    .build()
+            ))
+            .build();
+
+        taskDefinition.addContainer("HockeyteethApi", containerDefinitionOptions);
+
+        FargateService.Builder.create(this, "HockeyTeethAPIService")
+            .cluster(cluster)
+            .taskDefinition(taskDefinition)
+            .assignPublicIp(true)
+            .serviceName("hockeyteeth-api-service")
+            .build();
     }
 }
